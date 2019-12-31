dd4826a74c6934de45aeabc823e3f6a74500789d
Daniel Araujo <codkrdaniel@gmail.com>
2019-12-31 17:17:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php require 'header.php'; ?>
     <div class="container">
-        <h2>Usando checkboxes em PHP para iniciantes</h2>
         <form method="POST">
             <p>Quais suas linguagens de programação favoritas?</p>
             <input type="checkbox" value="PHP" name="languages[]"> PHP <br />
             <input type="checkbox" value="Java" name="languages[]"> Java <br />
-            <input type="checkbox" value="Csharp" name="languages[]"> C# <br />
-            <input type="checkbox" value="Delphi" name="languages[]"> Delphi <br />
-            <input type="submit" name="btnSend" class="btn btn-primary" value="Enviar dados" />
+            <input type="checkbox" value="C#" name="languages[]"> C# <br />
+            <input type="checkbox" value="Python" name="languages[]"> Python <br />
+            <input type="submit" name="btnSend" value="Enviar dados" />
         </form>
         <?php if($return): ?>
             <h4>Você selecionou as seguintes linguagens:</h4>
